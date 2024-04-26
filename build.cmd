5731b43d020d5d8e5dc129697318d6809eeb8012
Dele Olajide <dele@4ng.net>
2024-04-26 14:49:21 +0100

    fix #11 Plugin does not work on OF 4.8.1

--- build.cmd
+++ build.cmd
@@ -2,10 +2,10 @@ call mvn clean package -Dmaven.test.skip=true
 
 cd target
 rename rawpropertyeditor-openfire-plugin-assembly.jar rawpropertyeditor.jar
-rd "C:\openfire_4_6_2\plugins\rawpropertyeditor" /q /s
-del "C:\openfire_4_6_2\plugins\rawpropertyeditor.jar" 
-del /q "C:\openfire_4_6_2\logs\*.*"
-copy rawpropertyeditor.jar C:\openfire_4_6_2\plugins\rawpropertyeditor.jar
+rd "D:\Openfire\openfire_4_8_1\plugins\rawpropertyeditor" /q /s
+del "D:\Openfire\openfire_4_8_1\plugins\rawpropertyeditor.jar" 
+del /q "D:\Openfire\openfire_4_8_1\logs\*.*"
+copy rawpropertyeditor.jar D:\Openfire\openfire_4_8_1\plugins\rawpropertyeditor.jar
 
 
 pause
